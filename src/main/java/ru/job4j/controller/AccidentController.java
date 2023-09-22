777d937f4906f4f918df9bf84790b9027a647495
iiuhenio <iuheniomail@yandex.ru>
2023-09-22 12:37:34 +0300

    2. IndexController. Таблица и вид. [#2092 #446671]

--- src/main/java/ru/job4j/controller/AccidentController.java
+++ src/main/java/ru/job4j/controller/AccidentController.java
@@ -1,80 +1,39 @@
 package ru.job4j.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.model.Accident;
 import ru.job4j.service.AccidentService;
 
-
-import javax.servlet.http.HttpSession;
-
 @Controller
-@RequestMapping("/accidents") /* Работать с ДТП будем по URI /accidents/** */
+@AllArgsConstructor
 public class AccidentController {
+    private final AccidentService accidents;
 
-    private final AccidentService accidentService;
-
-    public AccidentController(AccidentService accidentService) {
-        this.accidentService = accidentService;
+    @GetMapping("/createAccident")
+    public String viewCreateAccident() {
+        return "createAccident";
     }
 
-    @GetMapping
-    public String getAll(Model model) {
-        model.addAttribute("accidents", accidentService.findAll());
-        return "accidents/list";
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident) {
+        accidents.create(accident);
+        return "redirect:/index";
     }
 
-    @GetMapping("/create")
-    public String getCreationPage(Model model) {
-        return "accidents/create";
-    }
-
-    @PostMapping("/create")
-    public String create(@ModelAttribute Accident accident, @RequestParam MultipartFile file, Model model) {
-        try {
-            accidentService.save(accident);
-            return "redirect:/accidents";
-        } catch (Exception exception) {
-            model.addAttribute("message", exception.getMessage());
-            return "errors/404";
-        }
-    }
-
-    @GetMapping("/{id}")
-    public String getById(Model model, @PathVariable int id, HttpSession session) {
-        var accidentOptional = accidentService.findById(id);
-        if (accidentOptional.isEmpty()) {
-            model.addAttribute("message", "ДТП с указанным идентификатором не найдено");
-            return "errors/404";
-        }
-        model.addAttribute("candidate", accidentOptional.get());
-        return "accidents/one";
-    }
-
-    @PostMapping("/update")
-    public String update(@ModelAttribute Accident accident, @RequestParam MultipartFile file, Model model) {
-        try {
-            var isUpdated = accidentService.update(accident);
+    @PostMapping("/editAccident")
+    public String edit(@ModelAttribute Accident accident, Model model) {
+            var isUpdated = accidents.update(accident);
             if (!isUpdated) {
-                model.addAttribute("message", "ДТП с указанным идентификатором не найдено");
+                model.addAttribute("message", "Инцидент с указанным идентификатором не найдено");
                 return "errors/404";
             }
-            return "redirect:/accidents";
-        } catch (Exception exception) {
-            model.addAttribute("message", exception.getMessage());
-            return "errors/404";
-        }
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(Model model, @PathVariable int id) {
-        var isDeleted = accidentService.deleteById(id);
-        if (!isDeleted) {
-            model.addAttribute("message", "ДТП с указанным идентификатором не найдено");
-            return "errors/404";
-        }
-        return "redirect:/accidents";
+            return "redirect:/index";
     }
 }
